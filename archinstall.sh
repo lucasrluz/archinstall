86fc1566694b9c6ab4c8a791bae5c229ff28dd9f
Lucas Ribeiro da Luz <78937360+lucasrluz@users.noreply.github.com>
2025-06-10 16:41:44 -0300

    feat: adicionado swap, formatação e montagem das partições

--- archinstall.sh
+++ archinstall.sh
@@ -1,7 +1,24 @@
 #!/bin/bash
 
+loadkeys br-abnt2
+
 DISCO="/dev/sda" 
 parted -s "$DISCO" mklabel gpt
+
 parted -s "$DISCO" mkpart ESP fat32 1MiB 501MiB
 parted -s "$DISCO" set 1 esp on
-parted -s "$DISCO" mkpart primary 501MiB 100%
+
+parted -s "$DISCO" mkpart primary linux-swap 501MiB 4550MiB
+
+parted -s "$DISCO" mkpart primary 4550MiB 100%
+
+mkfs.fat -F32 /dev/sda1
+mkswap /dev/sda2
+mkfs.ext4 /dev/sda3
+
+mount /dev/sda3 /mnt
+mkdir -p /mnt/boot/efi
+
+mount /dev/sda1 /mnt/boot/efi
+
+swapon /dev/sda2
