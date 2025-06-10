78e93c68849056f6a83d8a7d6033928b524eef45
Lucas Ribeiro da Luz <78937360+lucasrluz@users.noreply.github.com>
2025-06-10 16:46:08 -0300

    docs: adicionado comentários

--- archinstall.sh
+++ archinstall.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+# Teclado em português
 loadkeys br-abnt2
 
+# Layout do disco
 DISCO="/dev/sda" 
 parted -s "$DISCO" mklabel gpt
 
@@ -12,10 +14,12 @@ parted -s "$DISCO" mkpart primary linux-swap 501MiB 4550MiB
 
 parted -s "$DISCO" mkpart primary 4550MiB 100%
 
+# Formatação das partições
 mkfs.fat -F32 /dev/sda1
 mkswap /dev/sda2
 mkfs.ext4 /dev/sda3
 
+# Montagem do sistema de arquivo
 mount /dev/sda3 /mnt
 mkdir -p /mnt/boot/efi
 
