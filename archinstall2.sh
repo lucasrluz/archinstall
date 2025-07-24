4b01ff863f25345a08322d67c7705180b397026d
Lucas Ribeiro da Luz <78937360+lucasrluz@users.noreply.github.com>
2025-07-24 13:47:15 -0300

    feat: remoção da senha root

--- archinstall2.sh
+++ archinstall2.sh
@@ -5,9 +5,9 @@ ln -sf /usr/share/zoneinfo/America/Sao_Paulo /etc/localtime
 locale-gen
 echo "KEYMAP=br-abnt2" > /etc/vconsole.conf
 echo "archlinux" > /etc/hostname
-echo "root" | passwd -s
 
 # chroot > grub
 pacman -S grub efibootmgr
+
 grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
